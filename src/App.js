eb32b50b00e865533af160a30b77941a852d7566
Shubham <130773338+foreshubham@users.noreply.github.com>
2024-07-21 12:16:45 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import ImageResizer from './Component/ImageResizer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ImageResizer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
